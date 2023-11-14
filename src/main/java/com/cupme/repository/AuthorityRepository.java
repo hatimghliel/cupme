ed09fa134b8688b7c6054bf6d572b93d68dbf8da
mchekrouni <mchekrouni@sqli.com>
2023-11-14 14:00:58 +0100

    Initial version of CupMe generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/cupme/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.cupme.repository;
+
+import com.cupme.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
